fcd7b7c6232c75ca9746cdd9ed2acf7cd3eb08bd
VitorGreff <vitorgreffdias@gmail.com>
2024-05-19 21:27:56 -0300

    getting beaten by handlers

---
+++ src/db/data.ts
@@ -0,0 +1,24 @@
+export const vehicles = [
+    {
+        "Marca": "Toyota",
+        "Modelo": "Corolla",
+        "Cor": "Vermelho",
+        "Placa": "ABC1234",
+        "Tipo": "Sedan"
+    },
+    {
+        "Marca": "Ford",
+        "Modelo": "Fiesta",
+        "Cor": "Azul",
+        "Placa": "DEF5678",
+        "Tipo": "Hatchback"
+    },
+    {
+        "Marca": "Honda",
+        "Modelo": "Civic",
+        "Cor": "Preto",
+        "Placa": "GHI9012",
+        "Tipo": "Coupe"
+    }
+]
+
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,38 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { vehicles } from "./db/data";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+
+const app = new Elysia()
+  .all('/', () => 'hi, from elysia ;)')
+  .get('/vehicles', () => vehicles)
+  .guard({
+    params: t.Object({
+      id: t.Numeric()
+    })
+  })
+
+  .get('/vehicle/:id', ({ params: { id } }) => vehicles[id])
+  .post('/vehicle', ({ body }) => {
+    const vehicleSchema = t.Object({
+      Marca: t.String(),
+      Modelo: t.String(),
+      Cor: t.String(),
+      Placa: t.String(),
+      Tipo: t.String(),
+    });
+    if (!vehicleSchema.validate(body))
+        return {status: 400, body :'Invalid vehicle data'}
+    vehicles.push(body)
+
+  })
+  .put('/vehicle', () => 'vehicle altered')
+  .delete('/vehicle', () => 'vehicle deleted')
+
+  .listen(8080)
+
+// tests
+// app.handle(new Request('http://localhost/'))
+//   .then(console.log)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
